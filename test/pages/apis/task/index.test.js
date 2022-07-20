2db4888714b4d458b081c5703f9d71fe89d341e0
Pedro <pedroball0120@gmail.com>
2022-07-19 23:10:33 -0400

    jesttest

--- test/pages/apis/task/index.test.js
+++ test/pages/apis/task/index.test.js
@@ -1,8 +1,20 @@
 const request = require("supertest");
 const index = require("../../../../pages/api/task/index");
 
+const mockResponse = () => {
+    const res = {};
+    res.status = jest.fn(() => {
+        res = {}
+        res.send = jest.fn().mockReturnValue(res)
+        return res
+    }).mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    res.send = jest.fn().mockReturnValue(res);
+    return res;
+};
 describe("Test the index path", () => {
     test("It should response the GET method", () => {
+        mockResponse();
         return request(index)
           .get(`/?email=pedroball0120@gmail.com`)
           .expect(201);
